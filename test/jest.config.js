4123256ed78ef77c1b0a3b0084190d2387b439af
David Alekna <alekna@live.co.uk>
2021-06-04 15:26:18 +0100

    tests setup

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  displayName: 'final',
+  preset: 'ts-jest',
   roots: [path.join(__dirname, '../src')],
   rootDir: path.join(__dirname, '..'),
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/*.final.*'],
+  testMatch: ['**/__tests__/**'],
   moduleDirectories: ['node_modules', __dirname, path.join(__dirname, '../src')],
   coverageDirectory: path.join(__dirname, '../coverage'),
   collectCoverageFrom: ['**/src/**/*.js'],
   coveragePathIgnorePatterns: ['.*/__tests__/.*'],
   setupFilesAfterEnv: [require.resolve('./setup-env')],
-  watchPlugins: [require.resolve('jest-watch-typeahead/filename'), require.resolve('jest-watch-typeahead/testname')],
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
 };
