fdd50367bedacf307f79c901cbb38839a4a8583d
David Alekna <alekna@live.co.uk>
2021-06-04 15:29:15 +0100

    generate js

--- test/utils/generate.js
+++ test/utils/generate.js
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { getUserToken, getSaltAndHash } from '../../src/utils/auth';
+import { getUserToken } from '../../src/utils/auth';
 
 // passwords must have at least these kinds of characters to be valid, so we'll
 // prefex all of the ones we generate with `!0_Oo` to ensure it's valid.
@@ -13,53 +13,12 @@ function buildUser({ password = getPassword(), ...overrides } = {}) {
   return {
     id: getId(),
     username: getUsername(),
-    ...getSaltAndHash(password),
-    ...overrides,
-  };
-}
-
-function buildBook(overrides) {
-  return {
-    id: getId(),
-    title: faker.lorem.words(),
-    author: faker.name.findName(),
-    coverImageUrl: faker.image.imageUrl(),
-    pageCount: faker.random.number(400),
-    publisher: faker.company.companyName(),
-    synopsis: faker.lorem.paragraph(),
-    ...overrides,
-  };
-}
-
-function buildListItem(overrides = {}) {
-  const {
-    bookId = overrides.book ? overrides.book.id : getId(),
-    startDate = faker.date.past(2),
-    finishDate = faker.date.between(startDate, new Date()),
-    owner = { ownerId: getId() },
-  } = overrides;
-  return {
-    id: getId(),
-    bookId,
-    ownerId: owner.id,
-    rating: faker.random.number(5),
-    notes: faker.random.boolean() ? '' : getNotes(),
-    finishDate,
-    startDate,
     ...overrides,
   };
 }
 
 function token(user) {
   return getUserToken(buildUser(user));
-}
-
-function loginForm(overrides) {
-  return {
-    username: getUsername(),
-    password: getPassword(),
-    ...overrides,
-  };
 }
 
 function buildReq({ user = buildUser(), ...overrides } = {}) {
@@ -81,14 +40,11 @@ function buildNext(impl) {
 }
 
 export {
+  buildUser,
+  token,
   buildReq,
   buildRes,
   buildNext,
-  buildUser,
-  buildListItem,
-  buildBook,
-  token,
-  loginForm,
   getPassword as password,
   getUsername as username,
   getId as id,
