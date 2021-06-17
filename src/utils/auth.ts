4cd84a1414763f9957e3fde91517038f67823c5a
David Alekna <alekna@live.co.uk>
2021-06-17 13:56:17 +0100

    auth plugin

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,12 @@
-export const add = (a: number, b: number) => a + b;
+import { FastifyInstance } from 'fastify';
+import bearerAuthPlugin, { FastifyBearerAuthOptions } from 'fastify-bearer-auth';
 
-export const auth = () => {};
+// READ MORE: https://github.com/fastify/fastify-bearer-auth
+
+const defaultOptions: FastifyBearerAuthOptions = {
+  keys: new Set(['a-super-secret-key']),
+};
+
+export const registerAuth = (fastify: FastifyInstance) => {
+  fastify.register(bearerAuthPlugin, defaultOptions);
+};
